f1e897fea07193cefb929a9c62f15c3eb3f25811
Usuario Local <Usuario Local>
2017-03-28 17:44:43 +0200

    habanses

--- HolaSDL/StopOnBordersPhysics.cpp
+++ HolaSDL/StopOnBordersPhysics.cpp
@@ -6,6 +6,7 @@ StopOnBordersPhysics::StopOnBordersPhysics(bool left, bool right, bool top,
 }
 
 StopOnBordersPhysics::~StopOnBordersPhysics() {
+
 }
 
 void StopOnBordersPhysics::init(GameObject* o) {
@@ -13,4 +14,18 @@ void StopOnBordersPhysics::init(GameObject* o) {
 
 
 void StopOnBordersPhysics::update(GameObject* o) {
+	
+	if (o->getPosition().getX() + o->getDirection().getX < 0 && left_){
+		o->setDirectionX(0);
+		o->setPositionX(o->getPosition().getX);
+	}
+	if (o->getPosition().getX() + o->getDirection().getX > 480 && right_){
+
+	}
+	if (o->getPosition().getY() + o->getDirection().getY() < 0 && top_){
+
+	}
+	if (o->getPosition().getY() + o->getDirection().getY() > 640 && bottom_){
+
+	}
 }
